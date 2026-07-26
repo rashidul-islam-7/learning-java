8e817a3a1cdca4493af1d0c4e3a62017fda858ba
rashidulislamg71 <jrashidulislam0103@gmail.com>
2026-07-26 06:14:55 +0600

    my first java code

---
+++ Variable.java
@@ -0,0 +1,22 @@
+public class Variable {
+    /*
+     * A variable is a container that stores data under a name.
+     * Syntax:
+     * dataType variableName = value;
+     * int age = 20;
+     * String name = "Rashidul";
+     * double salary = 25000.50;
+     * boolean isStudent = true;
+     * char grade = 'A';
+     */
+
+    public static void main(String[] args) {
+        int roll = 816483;
+        String name = "Rashed";
+        double GPA = 3.90;
+
+        System.out.println(name);
+        System.out.println(roll);
+        System.out.println(GPA);
+    }
+}
